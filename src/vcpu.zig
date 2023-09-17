3982bfbc198774433062eb2779c4bdf2dece0d86
ShadowCurse <shadowcurse57@gmail.com>
2023-09-17 17:26:13 +0100

    vcpu get/set regs + running in separate thread + pause

--- src/vcpu.zig
+++ src/vcpu.zig
@@ -9,12 +9,49 @@ const Vm = @import("vm.zig").Vm;
 // ioctl in std uses c_int as a request type which is incorrect.
 extern "c" fn ioctl(fd: std.os.fd_t, request: c_ulong, ...) c_int;
 
+extern "c" fn __libc_current_sigrtmin() c_int;
+extern "c" fn pthread_kill(thread: std.c.pthread_t, sig: i32) std.c.E;
+
 const VcpuError = error{
     New,
     Init,
     SetReg,
+    GetReg,
     Run,
 };
+
+pub fn core_reg_id(comptime name: []const u8) u64 {
+    const offset = @offsetOf(KVM.kvm_regs, "regs") + @offsetOf(KVM.struct_user_pt_regs, name);
+    return KVM.KVM_REG_ARM64 | KVM.KVM_REG_SIZE_U64 | KVM.KVM_REG_ARM_CORE | (offset / @sizeOf(u32));
+}
+
+pub fn sys_reg_id(op0: u64, op1: u64, crn: u64, crm: u64, op2: u64) u64 {
+    return KVM.KVM_REG_ARM64 | KVM.KVM_REG_SIZE_U64 | KVM.KVM_REG_ARM64_SYSREG | ((op0 << KVM.KVM_REG_ARM64_SYSREG_OP0_SHIFT) & KVM.KVM_REG_ARM64_SYSREG_OP0_MASK) | ((op1 << KVM.KVM_REG_ARM64_SYSREG_OP1_SHIFT) & KVM.KVM_REG_ARM64_SYSREG_OP1_MASK) | ((crn << KVM.KVM_REG_ARM64_SYSREG_CRN_SHIFT) & KVM.KVM_REG_ARM64_SYSREG_CRN_MASK) | ((crm << KVM.KVM_REG_ARM64_SYSREG_CRM_SHIFT) & KVM.KVM_REG_ARM64_SYSREG_CRM_MASK) | ((op2 << KVM.KVM_REG_ARM64_SYSREG_OP2_SHIFT) & KVM.KVM_REG_ARM64_SYSREG_OP2_MASK);
+}
+
+threadlocal var v: ?*Vcpu = null;
+
+fn signal_handler(s: c_int) callconv(.C) void {
+    _ = s;
+    v.?.kvm_run.immediate_exit = 1;
+}
+
+pub fn set_thread_handler() !i32 {
+    const sig = __libc_current_sigrtmin();
+    const sigact = std.os.linux.Sigaction{
+        .handler = .{ .handler = signal_handler },
+        .flags = 4,
+        .mask = .{},
+        .restorer = null,
+    };
+    try std.os.sigaction(@intCast(sig), &sigact, null);
+    return sig;
+}
+
+pub fn kick_thread(thread: *const std.Thread, sig: i32) void {
+    const r = pthread_kill(thread.impl.handle, sig);
+    std.log.info("kick_thread: {}", .{r});
+}
 
 pub const Vcpu = struct {
     fd: std.os.fd_t,
@@ -26,6 +63,21 @@ pub const Vcpu = struct {
         target: u32,
         features: [7]u32,
     };
+
+    pub const PC = core_reg_id("pc");
+    pub const REGS0 = core_reg_id("regs");
+    pub const PSTATE = core_reg_id("pstate");
+    pub const MIDR_EL1 = sys_reg_id(3, 0, 0, 0, 0);
+
+    /// PSR (Processor State Register) bits.
+    /// Taken from arch/arm64/include/uapi/asm/ptrace.h.
+    const PSR_MODE_EL1h: u64 = 0x0000_0005;
+    const PSR_F_BIT: u64 = 0x0000_0040;
+    const PSR_I_BIT: u64 = 0x0000_0080;
+    const PSR_A_BIT: u64 = 0x0000_0100;
+    const PSR_D_BIT: u64 = 0x0000_0200;
+    /// Taken from arch/arm64/kvm/inject_fault.c.
+    pub const PSTATE_FAULT_BITS_64: u64 = PSR_MODE_EL1h | PSR_A_BIT | PSR_F_BIT | PSR_I_BIT | PSR_D_BIT;
 
     pub fn new(kvm: *const Kvm, vm: *const Vm, index: u64) !Self {
         const fd = ioctl(vm.fd, KVM.KVM_CREATE_VCPU, index);
@@ -54,35 +106,47 @@ pub const Vcpu = struct {
         }
     }
 
-    pub fn set_pc(self: *const Self, pc: u64) !void {
-        const offset = @offsetOf(KVM.kvm_regs, "regs") + @offsetOf(KVM.struct_user_pt_regs, "pc");
-        const pc_id = KVM.KVM_REG_ARM64 | KVM.KVM_REG_SIZE_U64 | KVM.KVM_REG_ARM_CORE | offset / @sizeOf(u32);
-        const kor: KVM.kvm_one_reg = .{ .id = pc_id, .addr = @intFromPtr(&pc) };
+    pub fn set_reg(self: *const Self, comptime t: type, reg_id: u64, value: t) !void {
+        std.log.info("setting reg: 0x{x} to 0x{x}", .{ reg_id, value });
+        const kor: KVM.kvm_one_reg = .{ .id = reg_id, .addr = @intFromPtr(&value) };
         const r = ioctl(self.fd, KVM.KVM_SET_ONE_REG, @intFromPtr(&kor));
         if (r < 0) {
             return VcpuError.SetReg;
         }
     }
 
-    pub fn set_fdt(self: *const Self, fdt_addr: u64) !void {
-        const offset = @offsetOf(KVM.kvm_regs, "regs") + @offsetOf(KVM.struct_user_pt_regs, "regs");
-        const regs_id = KVM.KVM_REG_ARM64 | KVM.KVM_REG_SIZE_U64 | KVM.KVM_REG_ARM_CORE | offset;
-        const kor: KVM.kvm_one_reg = .{ .id = regs_id, .addr = @intFromPtr(&fdt_addr) };
-        const r = ioctl(self.fd, KVM.KVM_SET_ONE_REG, @intFromPtr(&kor));
+    pub fn get_reg(self: *const Self, reg_id: u64) !u64 {
+        var value: u64 = undefined;
+        const kor: KVM.kvm_one_reg = .{ .id = reg_id, .addr = @intFromPtr(&value) };
+        const r = ioctl(self.fd, KVM.KVM_GET_ONE_REG, @intFromPtr(&kor));
         if (r < 0) {
-            return VcpuError.SetReg;
+            return VcpuError.GetReg;
         }
+        return value;
     }
 
     pub fn run(self: *const Self) !void {
         const r = ioctl(self.fd, KVM.KVM_RUN, @as(u32, 0));
-        if (r < 0) {
-            std.log.err("vcpu run error: {}", .{std.c.getErrno(r)});
+        // -4 == -EINTR - if vcpu was interrupted
+        if (r < 0 and r != -4) {
+            std.log.err("vcpu run error: {}:{}", .{ r, std.c.getErrno(r) });
             return VcpuError.Run;
         }
 
         switch (self.kvm_run.exit_reason) {
+            KVM.KVM_EXIT_IO => std.log.info("Got KVM_EXIT_IO", .{}),
+            KVM.KVM_EXIT_HLT => std.log.info("Got KVM_EXIT_HLT", .{}),
+            KVM.KVM_EXIT_MMIO => std.log.info("Got KVM_EXIT_MMIO: phys_addr: {x}, data: {any}, len: {}, is_write: {}", .{ self.kvm_run.unnamed_0.mmio.phys_addr, self.kvm_run.unnamed_0.mmio.data, self.kvm_run.unnamed_0.mmio.len, self.kvm_run.unnamed_0.mmio.is_write }),
             else => |exit| std.log.info("Got KVM_EXIT: {}", .{exit}),
         }
+
+        const pc = try self.get_reg(Self.PC);
+        std.log.info("pc: 0x{x}", .{pc});
+    }
+
+    pub fn run_threaded(self: *Self) !void {
+        v = self;
+        std.log.info("vcpu run", .{});
+        try self.run();
     }
 };
