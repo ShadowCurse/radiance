9ae7c90afa0e44265a3996ff0edca837f8158b7b
ShadowCurse <shadowcurse57@gmail.com>
2023-10-18 23:03:40 +0100

    fixing virtio context

--- src/virtio/context.zig
+++ src/virtio/context.zig
@@ -27,6 +27,7 @@ pub const MMIO_MAGIC_VALUE: u32 = 0x7472_6976;
 pub const MMIO_VERSION: u32 = 2;
 
 pub const VirtioAction = enum {
+    NoAction,
     ActivateDevice,
     ResetDevice,
     ConfigRead,
@@ -43,37 +44,39 @@ pub fn VirtioContext(comptime config_type: type) type {
         driver_features_word: u32,
         acked_features: u64,
 
+        device_type: u32,
         device_status: u32,
         interrupt_status: u32,
 
         config_blob: config_type,
-        activate_evt: EventFd,
+        config_generation: u32,
 
         selected_queue: u32,
         queue: Queue,
-        queue_evts: EventFd,
+        queue_event: EventFd,
 
         irq_status: std.atomic.Atomic(u32),
         irq_evt: EventFd,
 
         const Self = @This();
 
-        pub fn new() !Self {
+        pub fn new(device_type: u32) !Self {
             return Self{
                 .device_features_word = 0,
                 .avail_features = 0,
                 .driver_features_word = 0,
                 .acked_features = 0,
 
+                .device_type = device_type,
                 .device_status = 0,
                 .interrupt_status = 0,
 
                 .config_blob = undefined,
-                .activate_evt = try EventFd.new(0, nix.EFD_NONBLOCK),
+                .config_generation = 0,
 
                 .selected_queue = 0,
                 .queue = Queue.new(),
-                .queue_evts = try EventFd.new(0, nix.EFD_NONBLOCK),
+                .queue_event = try EventFd.new(0, nix.EFD_NONBLOCK),
 
                 .irq_status = std.atomic.Atomic(u32).init(0),
                 .irq_evt = try EventFd.new(0, nix.EFD_NONBLOCK),
@@ -81,7 +84,9 @@ pub fn VirtioContext(comptime config_type: type) type {
         }
 
         fn update_device_status(self: *Self, status: u32) ?VirtioAction {
-            switch (~self.device_status & status) {
+            const changed_bit = ~self.device_status & status;
+            log.info(@src(), "changin device status: {}", .{changed_bit});
+            switch (changed_bit) {
                 ACKNOWLEDGE => if (self.device_status == INIT) {
                     self.device_status = status;
                 },
@@ -105,8 +110,8 @@ pub fn VirtioContext(comptime config_type: type) type {
             }
         }
 
-        pub fn read(self: *Self, offset: u64, data: []u8) ?VirtioAction {
-            const data_u32: *u32 = @ptrCast(data.ptr);
+        pub fn read(self: *Self, offset: u64, data: []u8) VirtioAction {
+            const data_u32: *u32 = @ptrCast(@alignCast(data.ptr));
             switch (offset) {
                 0x0 => data_u32.* = MMIO_MAGIC_VALUE,
                 0x04 => data_u32.* = MMIO_VERSION,
@@ -118,13 +123,14 @@ pub fn VirtioContext(comptime config_type: type) type {
                         // Get the lower 32-bits of the features bitfield.
                         0 => (self.avail_features & 0xFFFFFFFF),
                         // Get the upper 32-bits of the features bitfield.
-                        1 => (self.avail_features >> 32) | 0x1,
+                        1 => self.avail_features >> 32,
+                        else => unreachable,
                     };
                     const features_u32: u32 = @truncate(features);
                     data_u32.* = features_u32;
                 },
-                0x34 => Queue.MAX_SIZE,
-                0x44 => self.queue.ready,
+                0x34 => data_u32.* = Queue.MAX_SIZE,
+                0x44 => data_u32.* = @intFromBool(self.queue.ready),
                 0x60 => {
                     data_u32.* = self.interrupt_status;
                 },
@@ -140,34 +146,42 @@ pub fn VirtioContext(comptime config_type: type) type {
                     log.warn(@src(), "invalid virtio read: offset: 0x{x}, data: {any}", .{ offset, data });
                 },
             }
+            return VirtioAction.NoAction;
         }
 
-        pub fn write(self: *Self, offset: u64, data: []u8) void {
-            const value_ptr: *u32 = @ptrCast(data.ptr);
+        pub fn write(self: *Self, offset: u64, data: []u8) VirtioAction {
+            const data_u32: *u32 = @ptrCast(@alignCast(data.ptr));
             switch (offset) {
-                0x14 => self.device_features_word = value_ptr.*,
+                0x14 => self.device_features_word = data_u32.*,
                 0x20 => {
+                    const value_u32 = data_u32.*;
+
+                    // TODO why shifting by 32 does not work
+                    var shifted = value_u32 << 16;
+                    shifted = shifted << 16;
+
                     switch (self.driver_features_word) {
-                        // Get the lower 32-bits of the features bitfield.
-                        0 => self.avail_features &= value_ptr.* & 0xFFFFFFFF,
-                        // Get the upper 32-bits of the features bitfield.
-                        1 => self.avail_features &= value_ptr.* << 32,
+                        // Set the lower 32-bits of the features bitfield.
+                        0 => self.avail_features = (self.avail_features & 0xffff_ffff_0000_0000) & value_u32,
+                        // Set the upper 32-bits of the features bitfield.
+                        1 => self.avail_features = (self.avail_features & 0x0000_0000_ffff_ffff) & shifted,
+                        else => unreachable,
                     }
                 },
-                0x24 => self.driver_features_word = value_ptr.*,
-                0x30 => self.selected_queue = value_ptr.*,
-                0x38 => self.queue.size = value_ptr.*,
-                0x44 => self.queue.ready = value_ptr.* == 1,
+                0x24 => self.driver_features_word = data_u32.*,
+                0x30 => self.selected_queue = data_u32.*,
+                0x38 => self.queue.size = data_u32.*,
+                0x44 => self.queue.ready = data_u32.* == 1,
                 0x64 => {
-                    self.interrupt_status &= ~value_ptr.*;
+                    self.interrupt_status &= ~data_u32.*;
                 },
-                0x70 => self.device_status = value_ptr.*,
-                0x80 => self.queue.set_desc_table(false, value_ptr.*),
-                0x84 => self.queue.set_desc_table(true, value_ptr.*),
-                0x90 => self.queue.set_avail_ring(false, value_ptr.*),
-                0x94 => self.queue.set_avail_ring(true, value_ptr.*),
-                0xa0 => self.queue.set_used_ring(false, value_ptr.*),
-                0xa4 => self.queue.set_used_ring(true, value_ptr.*),
+                0x70 => self.device_status = data_u32.*,
+                0x80 => self.queue.set_desc_table(false, data_u32.*),
+                0x84 => self.queue.set_desc_table(true, data_u32.*),
+                0x90 => self.queue.set_avail_ring(false, data_u32.*),
+                0x94 => self.queue.set_avail_ring(true, data_u32.*),
+                0xa0 => self.queue.set_used_ring(false, data_u32.*),
+                0xa4 => self.queue.set_used_ring(true, data_u32.*),
                 0x100...0xfff => {
                     const new_offset = offset - 0x100;
                     const config_blob_slice = std.mem.asBytes(&self.config_blob);
@@ -178,6 +192,7 @@ pub fn VirtioContext(comptime config_type: type) type {
                     log.warn(@src(), "invalid virtio write: offset: 0x{x}, data: {any}", .{ offset, data });
                 },
             }
+            return VirtioAction.NoAction;
         }
     };
 }
