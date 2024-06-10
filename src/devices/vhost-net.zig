87d07824d7cedf56627b7d5ca1d702c7f4f19ca6
ShadowCurse <shadowcurse@noreply.codeberg.org>
2024-06-10 23:05:52 +0100

    sort vhost-net features

--- src/devices/vhost-net.zig
+++ src/devices/vhost-net.zig
@@ -84,11 +84,11 @@ pub const VhostNet = struct {
         var virtio_context = try VirtioContext(2, VirtioNetConfig).new(TYPE_NET);
 
         virtio_context.avail_features =
-            1 << nix.VIRTIO_NET_F_GUEST_CSUM |
-            1 << nix.VIRTIO_NET_F_CSUM |
             1 << nix.VIRTIO_F_VERSION_1 |
             1 << nix.VIRTIO_RING_F_INDIRECT_DESC |
             1 << nix.VIRTIO_RING_F_EVENT_IDX |
+            1 << nix.VIRTIO_NET_F_GUEST_CSUM |
+            1 << nix.VIRTIO_NET_F_CSUM |
             1 << nix.VIRTIO_NET_F_GUEST_TSO4 |
             1 << nix.VIRTIO_NET_F_HOST_TSO4 |
             1 << nix.VIRTIO_NET_F_GUEST_TSO6 |
