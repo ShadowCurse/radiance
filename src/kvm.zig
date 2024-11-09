6ca85478867529bbf6d8d26c8bf9f980ce6043ab
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-11-09 12:05:58 +0000

    refactor kvm.zig

--- src/kvm.zig
+++ src/kvm.zig
@@ -1,24 +1,11 @@
-const std = @import("std");
 const nix = @import("nix.zig");
 
-file: std.fs.File,
+fd: nix.fd_t,
 
 const Self = @This();
 
-pub const KvmError = error{
-    KvmVersion,
-};
-
 pub fn new() !Self {
-    return .{ .file = try std.fs.openFileAbsolute("/dev/kvm", .{}) };
-}
-
-pub fn version(self: *const Self) !i32 {
-    return try nix.checked_ioctl(@src(), KvmError.KvmVersion, self.file.handle, nix.KVM_GET_API_VERSION, @as(usize, 0));
-    // const v = std.c.ioctl(self.file.handle, nix.KVM_GET_API_VERSION, @as(usize, 0));
-    // if (v < 0) {
-    //     return KvmError.KvmVersion;
-    // } else {
-    //     return v;
-    // }
+    return .{
+        .fd = try nix.open("/dev/kvm", .{}, 0),
+    };
 }
