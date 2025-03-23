05081898155c75e5c37974d53ca746afa0b344bf
ShadowCurse <shadowcurse@noreply.codeberg.org>
2025-03-23 22:41:21 +0000

    refactor: update the host cache info reading

--- src/cache.zig
+++ src/cache.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const log = @import("log.zig");
 
-pub const CacheType = enum {
+pub const CacheType = enum(u8) {
     Instruction,
     Data,
     Unified,
@@ -56,25 +56,25 @@ pub const CacheType = enum {
 pub const CacheEntry = struct {
     level: u8,
     cache_type: CacheType,
-    size: u64,
+    size: u32,
     cpus_per_unit: u16,
     line_size: u16,
     number_of_sets: u16,
 
     const Self = @This();
 
-    pub fn new(cache_dir: *const CacheDir, comptime index: usize) !Self {
+    pub fn new(cache_dir: *const std.fs.Dir, comptime index: usize) !Self {
         var read_buff: [30]u8 = undefined;
 
         // removing 1 from read bytes because last byte is `10` ASCII and
         // it breaks `parseInt`
-        const level_bytes = try cache_dir.read_info(index, "level", &read_buff) - 1;
+        const level_bytes = try Self.read_info(cache_dir, index, "level", &read_buff) - 1;
         const level = try std.fmt.parseInt(u8, read_buff[0..level_bytes], 10);
 
-        const cache_type_bytes = try cache_dir.read_info(index, "type", &read_buff) - 1;
+        const cache_type_bytes = try Self.read_info(cache_dir, index, "type", &read_buff) - 1;
         const cache_type = CacheType.from_str(read_buff[0..cache_type_bytes]);
 
-        const scm_bytes = try cache_dir.read_info(index, "shared_cpu_map", &read_buff) - 1;
+        const scm_bytes = try Self.read_info(cache_dir, index, "shared_cpu_map", &read_buff) - 1;
         var scm: u16 = 0;
         var scm_iter = std.mem.splitScalar(u8, read_buff[0..scm_bytes], ',');
         while (scm_iter.next()) |slice| {
@@ -82,13 +82,13 @@ pub const CacheEntry = struct {
             scm += @popCount(v);
         }
 
-        const cls_bytes = try cache_dir.read_info(index, "coherency_line_size", &read_buff) - 1;
+        const cls_bytes = try Self.read_info(cache_dir, index, "coherency_line_size", &read_buff) - 1;
         const cls = try std.fmt.parseInt(u16, read_buff[0..cls_bytes], 10);
 
-        const size_bytes = try cache_dir.read_info(index, "size", &read_buff) - 1;
+        const size_bytes = try Self.read_info(cache_dir, index, "size", &read_buff) - 1;
         const size = try parse_size(read_buff[0..size_bytes]);
 
-        const nos_bytes = try cache_dir.read_info(index, "number_of_sets", &read_buff) - 1;
+        const nos_bytes = try Self.read_info(cache_dir, index, "number_of_sets", &read_buff) - 1;
         const nos = try std.fmt.parseInt(u16, read_buff[0..nos_bytes], 10);
 
         return Self{
@@ -101,50 +101,52 @@ pub const CacheEntry = struct {
         };
     }
 
-    fn parse_size(s: []u8) !u64 {
+    fn parse_size(s: []u8) !u32 {
         return switch (s[s.len - 1]) {
-            'K' => try std.fmt.parseInt(u64, s[0 .. s.len - 1], 10) * 1024,
-            'M' => try std.fmt.parseInt(u64, s[0 .. s.len - 1], 10) * 1024 * 1024,
+            'K' => try std.fmt.parseInt(u32, s[0 .. s.len - 1], 10) * 1024,
+            'M' => try std.fmt.parseInt(u32, s[0 .. s.len - 1], 10) * 1024 * 1024,
             else => unreachable,
         };
     }
-};
-
-pub const CacheDir = struct {
-    const MAX_CACHE_LEVEL: u8 = 7;
-
-    dir: std.fs.Dir,
-
-    const Self = @This();
-
-    pub fn new() !Self {
-        const dir = try std.fs.openDirAbsolute("/sys/devices/system/cpu/cpu0/cache", .{});
-        return Self{
-            .dir = dir,
-        };
-    }
-
-    pub fn get_caches(dir: *const CacheDir) ![Self.MAX_CACHE_LEVEL]?CacheEntry {
-        var entries: [Self.MAX_CACHE_LEVEL]?CacheEntry = undefined;
-        inline for (0..Self.MAX_CACHE_LEVEL) |i| {
-            if (CacheEntry.new(dir, i)) |entry| {
-                entries[i] = entry;
-            } else |_| {
-                entries[i] = null;
-            }
-        }
-        return entries;
-    }
 
     fn read_info(
-        self: *const Self,
+        dir: *const std.fs.Dir,
         comptime index: u32,
         comptime name: []const u8,
         read_buff: []u8,
     ) !usize {
         const path = std.fmt.comptimePrint("index{d}/{s}", .{ index, name });
         log.debug(@src(), "reading path: {s}", .{path});
-        const file = try self.dir.openFile(path, .{});
+        const file = try dir.openFile(path, .{});
         return try file.read(read_buff);
     }
 };
+
+const MAX_CACHE_LEVEL: u8 = 3;
+pub const Caches = struct {
+    l1d_cache: ?CacheEntry = null,
+    l1i_cache: ?CacheEntry = null,
+    l2_cache: ?CacheEntry = null,
+    l3_cache: ?CacheEntry = null,
+};
+pub fn read_host_caches() !Caches {
+    const dir = try std.fs.openDirAbsolute("/sys/devices/system/cpu/cpu0/cache", .{});
+    var caches: Caches = .{};
+    inline for (0..MAX_CACHE_LEVEL) |i| {
+        const entry = try CacheEntry.new(&dir, i);
+        switch (entry.level) {
+            1 => {
+                switch (entry.cache_type) {
+                    .Data => caches.l1d_cache = entry,
+                    .Instruction => caches.l1i_cache = entry,
+                    .Unified => unreachable,
+                }
+            },
+            2 => caches.l2_cache = entry,
+            3 => caches.l3_cache = entry,
+            else => {},
+        }
+    }
+    log.debug(@src(), "Found caches: {any}", .{caches});
+    return caches;
+}
