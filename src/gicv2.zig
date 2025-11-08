b5cb768ce74b6b7e5ca77795d69066a282d8154c
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2025-11-08 21:29:12 +0000

    feat: add ability to save gicv2 state to the permenant memory

--- src/gicv2.zig
+++ src/gicv2.zig
@@ -34,7 +34,87 @@ pub const IRQ_MAX: u32 = 96;
 pub const MSIX_IRQ_BASE = 64;
 pub const MSIX_IRQ_NUM = 64;
 
-pub fn init(comptime System: type, vm: *const Vm) void {
+const Register = struct {
+    offset: u32,
+    len: u32,
+
+    fn with_byte_len(offset: u32, len: u32) Register {
+        return .{ .offset = offset, .len = len / @sizeOf(u32) };
+    }
+
+    fn with_bits_per_irq(offset: u32, bits: u32) Register {
+        return .{ .offset = offset, .len = bits * 1024 / 8 / @sizeOf(u32) };
+    }
+};
+
+// 4.1.3 CPU interface register map (page 76) in
+// https://developer.arm.com/documentation/ihi0048/latest/
+// Only RW regs. Also skip GICC_NSAPRn since it is only available when security
+// extensions present.
+// Kernel defines:
+// https://elixir.bootlin.com/linux/v6.17.7/source/include/linux/irqchip/arm-gic.h#L10
+const GIC_CPU_CTLR: Register = .with_byte_len(0x0, 4);
+const GIC_CPU_PRIMASK: Register = .with_byte_len(0x04, 4);
+const GIC_CPU_BINPOINT: Register = .with_byte_len(0x08, 4);
+const GIC_CPU_ALIAS_BINPOINT: Register = .with_byte_len(0x001c, 4);
+const GIC_CPU_ACTIVE_PRIO0: Register = .with_byte_len(0x00d0, 16);
+
+const VGIC_CPU_REGS = [_]Register{
+    GIC_CPU_CTLR,
+    GIC_CPU_PRIMASK,
+    GIC_CPU_BINPOINT,
+    GIC_CPU_ALIAS_BINPOINT,
+    GIC_CPU_ACTIVE_PRIO0,
+};
+
+// 4.1.3 CPU interface register map (page 75) in
+// https://developer.arm.com/documentation/ihi0048/latest/
+// Only RW regs
+// Kernel defines:
+// https://elixir.bootlin.com/linux/v6.17.7/source/include/linux/irqchip/arm-gic.h#L42
+const GIC_DIST_CTRL: Register = .with_byte_len(0x000, 12);
+const GIC_DIST_IGROUP: Register = .with_bits_per_irq(0x080, 1);
+const GIC_DIST_ENABLE_SET: Register = .with_bits_per_irq(0x100, 1);
+const GIC_DIST_ENABLE_CLEAR: Register = .with_bits_per_irq(0x180, 1);
+const GIC_DIST_PENDING_SET: Register = .with_bits_per_irq(0x200, 1);
+const GIC_DIST_PENDING_CLEAR: Register = .with_bits_per_irq(0x280, 1);
+const GIC_DIST_ACTIVE_SET: Register = .with_bits_per_irq(0x300, 1);
+const GIC_DIST_ACTIVE_CLEAR: Register = .with_bits_per_irq(0x380, 1);
+const GIC_DIST_PRI: Register = .with_bits_per_irq(0x400, 8);
+const GIC_DIST_CONFIG: Register = .with_bits_per_irq(0xc00, 2);
+const GIC_DIST_SGI_PENDING_CLEAR: Register = .with_byte_len(0xf10, 16);
+const GIC_DIST_SGI_PENDING_SET: Register = .with_byte_len(0xf20, 16);
+
+const VGIC_DIST_REGS = [_]Register{
+    GIC_DIST_CTRL,
+    GIC_DIST_IGROUP,
+    GIC_DIST_ENABLE_SET,
+    GIC_DIST_ENABLE_CLEAR,
+    GIC_DIST_PENDING_SET,
+    GIC_DIST_PENDING_CLEAR,
+    GIC_DIST_ACTIVE_SET,
+    GIC_DIST_ACTIVE_CLEAR,
+    GIC_DIST_PRI,
+    GIC_DIST_CONFIG,
+    GIC_DIST_SGI_PENDING_CLEAR,
+    GIC_DIST_SGI_PENDING_SET,
+};
+
+fn total_regs_entries(regs: []const Register) u32 {
+    var total: u32 = 0;
+    for (regs) |reg| total += reg.len;
+    return total;
+}
+
+const VGIC_INTERFACE_REGS_BYTES = total_regs_entries(&VGIC_CPU_REGS);
+const VGIC_DIST_REGS_BYTES = total_regs_entries(&VGIC_DIST_REGS);
+pub const State = [VGIC_INTERFACE_REGS_BYTES + VGIC_DIST_REGS_BYTES]u32;
+
+fd: nix.fd_t,
+
+const Self = @This();
+
+pub fn init(comptime System: type, vm: *const Vm) Self {
     var device: nix.kvm_create_device = .{
         .type = nix.KVM_DEV_TYPE_ARM_VGIC_V2,
         .fd = 0,
@@ -47,7 +127,7 @@ pub fn init(comptime System: type, vm: *const Vm) void {
     });
     const fd: nix.fd_t = @intCast(device.fd);
 
-    set_attributes(
+    set_attribute(
         System,
         fd,
         0,
@@ -55,7 +135,7 @@ pub fn init(comptime System: type, vm: *const Vm) void {
         nix.KVM_VGIC_V2_ADDR_TYPE_DIST,
         @intFromPtr(&Memory.GICV2_DISTRIBUTOR_ADDRESS),
     );
-    set_attributes(
+    set_attribute(
         System,
         fd,
         0,
@@ -65,7 +145,7 @@ pub fn init(comptime System: type, vm: *const Vm) void {
     );
     // KVM_DEV_ARM_VGIC_GRP_NR_IRQS sets the highest SPI interrupt number.
     // Total number of available interrupts is: IRQ_MAX - IRQ_BASE
-    set_attributes(
+    set_attribute(
         System,
         fd,
         0,
@@ -73,7 +153,7 @@ pub fn init(comptime System: type, vm: *const Vm) void {
         0,
         @intFromPtr(&IRQ_MAX),
     );
-    set_attributes(
+    set_attribute(
         System,
         fd,
         0,
@@ -81,9 +161,93 @@ pub fn init(comptime System: type, vm: *const Vm) void {
         nix.KVM_DEV_ARM_VGIC_CTRL_INIT,
         0,
     );
+
+    return .{ .fd = fd };
 }
 
-fn set_attributes(
+pub fn save_state(self: *const Self, comptime System: type, state: *State) void {
+    var current_reg: u32 = 0;
+    for (VGIC_CPU_REGS) |reg| {
+        for (0..reg.len) |i| {
+            get_attribute(
+                System,
+                self.fd,
+                0,
+                nix.KVM_DEV_ARM_VGIC_GRP_CPU_REGS,
+                reg.offset + i * @sizeOf(u32),
+                @intFromPtr(&state[current_reg]),
+            );
+            log.debug(
+                @src(),
+                "Reading GICv2 cpu reg: 0x{x} value: 0x{x}",
+                .{ reg.offset + i * @sizeOf(u32), state[current_reg] },
+            );
+            current_reg += 1;
+        }
+    }
+
+    for (VGIC_DIST_REGS) |reg| {
+        for (0..reg.len) |i| {
+            get_attribute(
+                System,
+                self.fd,
+                0,
+                nix.KVM_DEV_ARM_VGIC_GRP_DIST_REGS,
+                reg.offset + i * @sizeOf(u32),
+                @intFromPtr(&state[current_reg]),
+            );
+            log.debug(
+                @src(),
+                "Reading GICv2 dist reg: 0x{x} value: 0x{x}",
+                .{ reg.offset + i * @sizeOf(u32), state[current_reg] },
+            );
+            current_reg += 1;
+        }
+    }
+}
+
+pub fn restore_state(self: *const Self, comptime System: type, state: *const State) void {
+    var current_reg: u32 = 0;
+    for (VGIC_CPU_REGS) |reg| {
+        for (0..reg.len) |i| {
+            set_attribute(
+                System,
+                self.fd,
+                0,
+                nix.KVM_DEV_ARM_VGIC_GRP_CPU_REGS,
+                reg.offset + i * @sizeOf(u32),
+                @intFromPtr(&state[current_reg]),
+            );
+            log.debug(
+                @src(),
+                "Setting GICv2 cpu reg: 0x{x} value: 0x{x}",
+                .{ reg.offset + i * @sizeOf(u32), state[current_reg] },
+            );
+            current_reg += 1;
+        }
+    }
+
+    for (VGIC_DIST_REGS) |reg| {
+        for (0..reg.len) |i| {
+            set_attribute(
+                System,
+                self.fd,
+                0,
+                nix.KVM_DEV_ARM_VGIC_GRP_DIST_REGS,
+                reg.offset + i * @sizeOf(u32),
+                @intFromPtr(&state[current_reg]),
+            );
+            log.debug(
+                @src(),
+                "Setting GICv2 dist reg: 0x{x} value: 0x{x}",
+                .{ reg.offset + i * @sizeOf(u32), state[current_reg] },
+            );
+            current_reg += 1;
+        }
+    }
+}
+
+fn set_attribute(
     comptime System: type,
     fd: nix.fd_t,
     flags: u32,
@@ -101,6 +265,28 @@ fn set_attributes(
     _ = nix.assert(@src(), System, "ioctl", .{
         fd,
         nix.KVM_SET_DEVICE_ATTR,
+        @intFromPtr(&kda),
+    });
+}
+
+fn get_attribute(
+    comptime System: type,
+    fd: nix.fd_t,
+    flags: u32,
+    group: u32,
+    attr: u64,
+    addr: u64,
+) void {
+    const kda = nix.kvm_device_attr{
+        .flags = flags,
+        .group = group,
+        .attr = attr,
+        .addr = addr,
+    };
+    log.debug(@src(), "Getting device attributes: {any}", .{kda});
+    _ = nix.assert(@src(), System, "ioctl", .{
+        fd,
+        nix.KVM_GET_DEVICE_ATTR,
         @intFromPtr(&kda),
     });
 }
